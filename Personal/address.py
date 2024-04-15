534283c29953a849d7351291d9709820802da5ce
chrisk0312 <kangjt0312@gmail.com>
2024-04-15 18:02:45 +0900

    0415

--- Personal/address.py
+++ Personal/address.py
@@ -7,3 +7,32 @@ def load_data(filename = "C:\\study\\ERP\\SAP_bike_sales(datasets)\\Addresses.cs
             print(row)  # Replace this with code to insert the data into your ERP system
 
 load_data()
+
+#Creating a chatbot involves natural language processing
+
+import nltk
+from nltk.chat.util import Chat, reflections
+
+pairs = [
+    [
+        r"my name is (.*)",
+        ["Hello %1, How are you today ?",],
+    ],
+    [
+        r"hi|hey|hello",
+        ["Hello", "Hey there",],
+    ],
+    [
+        r"quit",
+        ["Bye. It was nice talking to you. See you soon :)"]
+    ],
+]
+
+def chatbot():
+    print("Hi, I'm your chatbot. You can start a conversation with me now.")
+
+    chat = Chat(pairs, reflections)
+    chat.converse()
+
+if __name__ == "__main__":
+    chatbot()
