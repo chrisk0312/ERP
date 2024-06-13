bdb9125c1298f7661719c46c0b6d861a90520901
chrisk0312 <kangjt0312@gmail.com>
2024-06-13 12:05:49 +0900

    0613

--- ERP/flask/ERPaddress.py
+++ ERP/flask/ERPaddress.py
@@ -1,9 +1,28 @@
 from flask import Flask, render_template, request
+import pandas as pd
 app = Flask(__name__)
+
+dataset =pd.read_csv('C:\study\ERP_chatbot\ERP\datasets\Address.csv')
 
 @app.route('/')
 def home():
     return render_template('Address.html')
+
+@app.route('/search/', methods=['GET', 'POST'])
+def search():
+    if request.method == 'POST':
+        search_id = request.form['id']
+        # Assuming 'Address ID' is the column name in your CSV
+        result = dataset[dataset['Address ID'] == int(search_id)]
+        if not result.empty:
+            # Convert the result to HTML or a format that can be easily displayed
+            result_html = result.to_html()
+            return render_template('Address.html', result=result_html)
+        else:
+            return "No matching address found."
+    else:
+        return render_template('Address.html')
+
 
 @app.route('/create/')
 def create():
