6991bec7a8b80fd33980d974c86d5c7fb60a0c54
chrisk0312 <kangjt0312@gmail.com>
2024-06-17 12:00:51 +0900

    0617

--- ERP/flask/ERPapp.py
+++ ERP/flask/ERPapp.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, send_file, request
 from PyPDF2 import PdfReader
 
 app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 @app.route('/')
 def home():
